697955c12169410f21ec41c9d3f030b6a5c8fa27
Arturo Vazquez <vazart68@gmail.com>
2023-08-26 13:07:57 -0600

    add function to login and register

--- VSC_art/script.js
+++ VSC_art/script.js
@@ -7,14 +7,29 @@ const buttonRegister = document.getElementById('registrar');
 const messageStatus = document.getElementById('mensaje');
 const bold = document.getElementById('bold');
 
-
+let users = [];
+class user{
+    constructor(name, password){
+        this.name=name;
+        this.password=password;
+    }
+}
 function login(){
     messageStatus.textContent='';
     bold.textContent='';
     if(inputUser.value != '' || inputPassword.value!=''){
-        messageStatus.textContent = 'Login correcto '; 
-        bold.textContent=inputUser.value;
-        inputPassword.value = '';
+        for(let i = 0; i<users.length;i++){
+            if(users[i].name==inputUser.value &&users[i].password==inputPassword.value){
+                messageStatus.textContent = 'Login correcto '; 
+                bold.textContent=inputUser.value;
+                inputPassword.value = '';
+                return
+            }
+            
+        }
+        messageStatus.textContent = "login incorrecto";
+        
+        
     }
 
 }
@@ -22,9 +37,14 @@ function register(){
     messageStatus.textContent = '';
     bold.textContent='';
     if(inputUser.value != '' || inputPassword.value!=''){
+        newUser = new user(inputUser.value,inputPassword.value);
+        users.push(newUser);
         messageStatus.textContent = 'Registro con exito ';
         bold.textContent=inputUser.value;
         inputPassword.value = '';
+    }
+    else{
+        messageStatus.textContent ='faltan campos por llenar';
     }
 }
 buttonLogin.addEventListener('click', login);
