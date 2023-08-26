23489c0f3ce2b451e4231cf51dc897ec0823f813
Arturo Vazquez <vazart68@gmail.com>
2023-08-26 12:53:34 -0600

    practica 3: Mensaje de login y registro

--- VSC_art/script.js
+++ VSC_art/script.js
@@ -1,9 +1,31 @@
-const miPrimerVariable = document.getElementById("txt")
-const button = document.getElementById("btn")
-const limpiar = document.getElementById("clean")
-button.addEventListener("click",function(){
-    alert(miPrimerVariable.value)
-})
-limpiar.addEventListener("click",function(){
-    miPrimerVariable.value=""
-})
+const inputUser = document.getElementById('user');
+const inputPassword = document.getElementById('password');
+
+const buttonLogin = document.getElementById('login');
+const buttonRegister = document.getElementById('registrar');
+
+const messageStatus = document.getElementById('mensaje');
+const bold = document.getElementById('bold');
+
+
+function login(){
+    messageStatus.textContent='';
+    bold.textContent='';
+    if(inputUser.value != '' || inputPassword.value!=''){
+        messageStatus.textContent = 'Login correcto '; 
+        bold.textContent=inputUser.value;
+        inputPassword.value = '';
+    }
+
+}
+function register(){
+    messageStatus.textContent = '';
+    bold.textContent='';
+    if(inputUser.value != '' || inputPassword.value!=''){
+        messageStatus.textContent = 'Registro con exito ';
+        bold.textContent=inputUser.value;
+        inputPassword.value = '';
+    }
+}
+buttonLogin.addEventListener('click', login);
+buttonRegister.addEventListener('click',register);
