6b13362bf3e28bf74e7934f305fe8d3c1fe30dce
Arturo Vazquez <vazart68@gmail.com>
2023-09-30 13:39:12 -0600

    Editar archivo

--- miApp-1959737/src/app/lista-alumnos/lista-alumnos.component.ts
+++ miApp-1959737/src/app/lista-alumnos/lista-alumnos.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-lista-alumnos',
   templateUrl: './lista-alumnos.component.html',
   styleUrls: ['./lista-alumnos.component.scss'],
 })
 export class ListaAlumnosComponent  implements OnInit {
-
+  alumnos:any = ["Carlos", "Ximena", "Alberto", "Eduardo", "Baraba", "Georgina", "Kevin"];
   constructor() { }
 
   ngOnInit() {}
