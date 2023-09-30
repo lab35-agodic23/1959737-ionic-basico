b18932f29675e39efe337862f792dad36324fd5e
Arturo Vazquez <vazart68@gmail.com>
2023-09-30 12:53:00 -0600

    Add setup project

---
+++ miApp-1959737/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'miApp-1959737',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
